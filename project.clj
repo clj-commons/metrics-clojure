a764383ee19c153dda20da9478e8eb25fb8baa62
Steve Losh <steve@stevelosh.com>
2011-11-02 18:21:42 -0400

    Bump version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject metrics-clojure "0.4.0"
+(defproject metrics-clojure "0.5.0"
   :description "A Clojure façade for Coda Hale's metrics library."
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [com.yammer.metrics/metrics-core "2.0.0-BETA17"]]
