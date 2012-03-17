964a31d753213dc9c0a2d73b90b1f1a5de00a2fd
Steve Losh <steve@stevelosh.com>
2012-03-17 17:15:32 -0400

    Increment version number.

--- metrics-clojure-ring/project.clj
+++ metrics-clojure-ring/project.clj
@@ -1,4 +1,4 @@
-(defproject metrics-clojure-ring "0.7.1"
+(defproject metrics-clojure-ring "0.8.0"
   :description "Various things gluing together metrics-clojure and ring."
   :dependencies [[cheshire "2.2.2"]
-                 [metrics-clojure "0.7.1"]])
+                 [metrics-clojure "0.8.0"]])
