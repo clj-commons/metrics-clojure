aa8227aaf0a6e8d667ec5bc4bdcb51a546405015
Michael Klishin <michael@defprotocol.org>
2013-03-22 10:48:42 +0400

    Upgrade to Metrics 2.2

--- metrics-clojure-core/project.clj
+++ metrics-clojure-core/project.clj
@@ -1,6 +1,6 @@
 (defproject metrics-clojure "0.10.0-SNAPSHOT"
   :description "A Clojure façade for Coda Hale's metrics library."
   :dependencies [[org.clojure/clojure "[1.2.1,1.4.0]"]
-                 [com.yammer.metrics/metrics-core "2.0.1"]]
+                 [com.yammer.metrics/metrics-core "2.2.0"]]
   :repositories {"repo.codahale.com" "http://repo.codahale.com"}
   :warn-on-reflection true)
