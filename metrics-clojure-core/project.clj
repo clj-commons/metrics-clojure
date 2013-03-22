f0a52b0693eca5b8317a2c8d569888923b393f21
Michael Klishin <michael@defprotocol.org>
2013-03-22 10:54:51 +0400

    Test against Clojure 1.3-1.6
    1.2.1 can be added the same way as needed, but it hardly matters
    much these days.

--- metrics-clojure-core/project.clj
+++ metrics-clojure-core/project.clj
@@ -2,5 +2,18 @@
   :description "A Clojure façade for Coda Hale's metrics library."
   :dependencies [[org.clojure/clojure "[1.2.1,1.4.0]"]
                  [com.yammer.metrics/metrics-core "2.2.0"]]
-  :repositories {"repo.codahale.com" "http://repo.codahale.com"}
+  :repositories {"repo.codahale.com" "http://repo.codahale.com"
+                 ;; to get Clojure snapshots
+                 "sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
+                             :snapshots false
+                             :releases {:checksum :fail :update :always}}
+                 "sonatype-snapshots" {:url "http://oss.sonatype.org/content/repositories/snapshots"
+                                       :snapshots true
+                                       :releases {:checksum :fail :update :always}}}
+    :profiles {:dev    {}
+               :1.3    {:dependencies [[org.clojure/clojure "1.3.0"]]}
+               :1.4    {:dependencies [[org.clojure/clojure "1.4.0"]]}
+               :1.6    {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}
+               :master {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}}
+  :aliases  {"all" ["with-profile" "dev:dev,1.3:dev,1.4:dev,1.6:dev,master"]}
   :warn-on-reflection true)
