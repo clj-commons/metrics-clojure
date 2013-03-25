e45b7e1ae5de2815090a5e4886d751fbb34351b4
Michael Klishin <michael@defprotocol.org>
2013-03-25 15:14:55 +0400

    Now working on 0.11.0-SNAPSHOT
    Take 2

--- metrics-clojure-ring/project.clj
+++ metrics-clojure-ring/project.clj
@@ -1,4 +1,4 @@
-(defproject metrics-clojure-ring "0.10.0"
+(defproject metrics-clojure-ring "0.11.0-SNAPSHOT"
   :description "Various things gluing together metrics-clojure and ring."
   :dependencies [[cheshire        "5.0.2"]
-                 [metrics-clojure "0.10.0"]])
+                 [metrics-clojure "0.11.0-SNAPSHOT"]])
