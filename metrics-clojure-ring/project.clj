06a2064548b1a2bedd81259afcbc9a8f72ea8099
Michael Klishin <michael@defprotocol.org>
2013-03-26 18:46:23 +0400

    Back to development version

--- metrics-clojure-ring/project.clj
+++ metrics-clojure-ring/project.clj
@@ -1,4 +1,4 @@
-(defproject metrics-clojure-ring "1.0.1"
+(defproject metrics-clojure-ring "1.1.0-SNAPSHOT"
   :description "Various things gluing together metrics-clojure and ring."
   :dependencies [[cheshire "5.0.2"]
-                 [metrics-clojure "1.0.1"]])
+                 [metrics-clojure "1.1.0-SNAPSHOT"]])
