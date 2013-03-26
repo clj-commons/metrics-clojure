beedb08702d344fb516ca10a98e08912ae1d611b
Michael Klishin <michael@defprotocol.org>
2013-03-26 18:43:48 +0400

    1.0.1

--- metrics-clojure-ring/project.clj
+++ metrics-clojure-ring/project.clj
@@ -1,4 +1,4 @@
-(defproject metrics-clojure-ring "1.0.0"
+(defproject metrics-clojure-ring "1.0.1"
   :description "Various things gluing together metrics-clojure and ring."
   :dependencies [[cheshire "5.0.2"]
-                 [metrics-clojure "1.0.0"]])
+                 [metrics-clojure "1.0.1"]])
