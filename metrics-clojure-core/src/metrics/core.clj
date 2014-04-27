2ea88520dea284f3f5ca5395fb8a30f0db9e7fb0
Michael Klishin <michael@clojurewerkz.org>
2014-04-27 18:03:43 +0400

    Counters test now passes

--- metrics-clojure-core/src/metrics/core.clj
+++ metrics-clojure-core/src/metrics/core.clj
@@ -1,19 +1,24 @@
 (ns metrics.core
-  (:require [metrics.utils :refer [metric-name]])
-  (:import com.yammer.metrics.Metrics
-           java.util.concurrent.TimeUnit
-           com.yammer.metrics.reporting.ConsoleReporter))
+  (:import [com.codahale.metrics MetricRegistry]))
 
+(def ^{:tag "MetricRegistry" :doc "Default registry used by public API functions when no explicit registry argument is given"}
+  default-registry
+  (MetricRegistry.))
+
+(defn ^String metric-name
+  [title]
+  (if (string? title)
+    (MetricRegistry/name "default"
+                         ^"[Ljava.lang.String;" (into-array String ["default" ^String title]))
+    (MetricRegistry/name
+     ^String (first title)
+     ^"[Ljava.lang.String;" (into-array String
+                                        [(second title)
+                                         (last title)]))))
 
 (defn remove-metric
   "Remove the metric with the given title."
-  [title]
-  (.removeMetric (Metrics/defaultRegistry) (metric-name title)))
-
-
-(defn report-to-console
-  "Report all metrics to standard out every few seconds."
-  [seconds]
-  (ConsoleReporter/enable seconds TimeUnit/SECONDS))
-
-
+  ([title]
+     (remove-metric default-registry title))
+  ([^MetricRegistry reg title]
+     (.remove reg (metric-name title))))
--- metrics-clojure-core/src/metrics/utils.clj
+++ metrics-clojure-core/src/metrics/utils.clj
@@ -1,44 +1,37 @@
 (ns metrics.utils
-  (:import com.yammer.metrics.Metrics
-           [com.yammer.metrics.core Sampling MetricName]))
+  (:require [metrics.core :refer [default-registry]])
+  (:import [com.codahale.metrics MetricRegistry Sampling]))
 
 
-(defn get-percentile [^Sampling metric ^double percentile]
+(defn get-percentile
+  [^Sampling metric ^double percentile]
   (-> metric
-    .getSnapshot
-    (.getValue percentile)))
+      .getSnapshot
+      (.getValue percentile)))
 
-(defn get-percentiles [metric percentiles]
+(defn get-percentiles
+  [metric percentiles]
   (zipmap percentiles
           (map (partial get-percentile metric)
                percentiles)))
 
-(defn ^MetricName metric-name [title]
-  (if (string? title)
-    (new MetricName "default" "default" ^String title)
-    (new MetricName
-         ^String (first title)
-         ^String (second title)
-         ^String (last title))))
+(defn metric-name
+  [title]
+  (metrics.core/metric-name title))
 
-(defn all-metrics []
-  (letfn [(parse-name [^MetricName metric-name]
-            (format "%s.%s.%s"
-                    (.getGroup metric-name)
-                    (.getType metric-name)
-                    (.getName metric-name)))
-          (parse-entry [[metric-name metric]]
-            [(parse-name metric-name)
-             metric])]
-    (into {} (map parse-entry (.allMetrics (Metrics/defaultRegistry))))))
+(defn all-metrics
+  ([]
+     (all-metrics default-registry))
+  ([^MetricRegistry reg]
+     (into {} (.getMetrics reg))))
 
 (defn desugared-title
   "Syntactic sugary goodness for defining metrics concisely with macros.
 
-  Returns a vector of: [symbol-to-define MetricName]"
+  Returns a vector of: [symbol-to-define metric-name]"
   [mn]
   (cond
-    (string? mn) [(symbol mn) mn]
-    (symbol? mn) [mn (str mn)]
-    :else [(symbol (last mn))
-           (map str mn)]))
+   (string? mn) [(symbol mn) mn]
+   (symbol? mn) [mn (str mn)]
+   :else [(symbol (last mn))
+          (map str mn)]))
