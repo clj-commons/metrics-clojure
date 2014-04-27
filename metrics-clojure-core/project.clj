5dd0aad8f1f34ead3a19e4e54323f1140b8b1f3d
Michael Klishin <michael@clojurewerkz.org>
2014-04-27 16:55:30 +0400

    master is now 1.7.0-master-SNAPSHOT

--- metrics-clojure-core/project.clj
+++ metrics-clojure-core/project.clj
@@ -12,6 +12,6 @@
                                        :releases {:checksum :fail :update :always}}}
     :profiles {:1.4    {:dependencies [[org.clojure/clojure "1.4.0"]]}
                :1.5    {:dependencies [[org.clojure/clojure "1.5.1"]]}
-               :master {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}}
+               :master {:dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]]}}
   :aliases  {"all" ["with-profile" "+dev:+1.4:+1.5:+master"]}
   :global-vars {*warn-on-reflection* true})
