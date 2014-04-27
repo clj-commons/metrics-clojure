4a125ca91cbaf44b59a9d89fac07b88b3e624384
Michael Klishin <michael@clojurewerkz.org>
2014-04-27 18:37:29 +0400

    Meters test now passes

--- metrics-clojure-core/test/metrics/test/meters-test.clj
+++ metrics-clojure-core/test/metrics/test/meters-test.clj
@@ -1,9 +1,9 @@
 (ns metrics.test.meters
-  (:require [metrics.meters :as meters])
-  (:use [metrics.test.test-utils])
-  (:use [clojure.test]))
+  (:require [metrics.meters :as meters]
+            [metrics.test.test-utils :refer :all]
+            [clojure.test :refer :all]))
 
-(meters/defmeter ["test" "meters" "defmetered"] "test-events")
+(meters/defmeter ["test" "meters" "defmetered"])
 
 (deftest test-defmeter
   (is (= (meters/rate-mean defmetered) 0.0))
@@ -11,47 +11,47 @@
   (is (> (meters/rate-mean defmetered) 0.0)))
 
 (deftest test-rate-mean
-  (let [m (meters/meter ["test" "meters" "test-rate-mean"] "test-events")]
+  (let [m (meters/meter ["test" "meters" "test-rate-mean"])]
     (is (= (meters/rate-mean m) 0.0))
     (meters/mark! m)
     (is (> (meters/rate-mean m) 0.0))))
 
 (deftest test-rate-mean-update-multiple
-  (let [m (meters/meter ["test" "meters" "test-rate-mean-update-multiple"] "test-events")]
+  (let [m (meters/meter ["test" "meters" "test-rate-mean-update-multiple"])]
     (is (= (meters/rate-mean m) 0.0))
     (meters/mark! m 10)
     (is (> (meters/rate-mean m) 0.0))))
 
 (deftest test-rate-one
-  (let [m (meters/meter ["test" "meters" "test-rate-one"] "test-events")]
+  (let [m (meters/meter ["test" "meters" "test-rate-one"])]
     (is (= (meters/rate-one m) 0.0))
     (meters/mark! m 20000)
     (Thread/sleep 8000)
     (is (> (meters/rate-one m) 0.0))))
 
 (deftest test-rate-five
-  (let [m (meters/meter ["test" "meters" "test-rate-five"] "test-events")]
+  (let [m (meters/meter ["test" "meters" "test-rate-five"])]
     (is (= (meters/rate-five m) 0.0))
     (meters/mark! m 20000)
     (Thread/sleep 8000)
     (is (> (meters/rate-five m) 0.0))))
 
 (deftest test-rate-fifteen
-  (let [m (meters/meter ["test" "meters" "test-rate-fifteen"] "test-events")]
+  (let [m (meters/meter ["test" "meters" "test-rate-fifteen"])]
     (is (= (meters/rate-fifteen m) 0.0))
     (meters/mark! m 20000)
     (Thread/sleep 8000)
     (is (> (meters/rate-fifteen m) 0.0))))
 
 (deftest test-rates
-  (let [m (meters/meter ["test" "meters" "test-rates"] "test-events")]
+  (let [m (meters/meter ["test" "meters" "test-rates"])]
     (is (every? zero? (vals (meters/rates m))))
     (meters/mark! m 20000)
     (Thread/sleep 8000)
     (is (every? #(> % 0.0) (vals (meters/rates m))))))
 
 (deftest test-count
-  (let [m (meters/meter ["test" "meters" "test-count"] "test-events")]
+  (let [m (meters/meter ["test" "meters" "test-count"])]
     (is (zero? (meters/count m)))
     (meters/mark! m)
     (is (= 1 (meters/count m)))))
