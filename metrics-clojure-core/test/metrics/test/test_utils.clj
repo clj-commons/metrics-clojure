26bb4d43d9216b0daa24efec495de427d0c018d8
Michael Klishin <michael@clojurewerkz.org>
2014-04-27 22:23:16 +0400

    Bring back some test helpers

--- metrics-clojure-core/test/metrics/test/test_utils.clj
+++ metrics-clojure-core/test/metrics/test/test_utils.clj
@@ -5,6 +5,14 @@
   [n]
   (if (> n 0) n (* -1 n)))
 
+(defn within-one [a b]
+  (<= (abs (- a b))
+      1))
+
+(defn within-ten [a b]
+  (<= (abs (- a b))
+      10))
+
 (defn maps-within-one
   [a b]
   (when (= (set (keys a)) (set (keys b)))
