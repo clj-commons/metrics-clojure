d2ba24293c27c8ab39c759e165b86c6e56819985
Michael Klishin <michael@clojurewerkz.org>
2014-04-27 16:55:20 +0400

    Drop 1.3 support

--- metrics-clojure-core/project.clj
+++ metrics-clojure-core/project.clj
@@ -10,9 +10,8 @@
                  "sonatype-snapshots" {:url "http://oss.sonatype.org/content/repositories/snapshots"
                                        :snapshots true
                                        :releases {:checksum :fail :update :always}}}
-    :profiles {:1.3    {:dependencies [[org.clojure/clojure "1.3.0"]]}
-               :1.4    {:dependencies [[org.clojure/clojure "1.4.0"]]}
+    :profiles {:1.4    {:dependencies [[org.clojure/clojure "1.4.0"]]}
                :1.5    {:dependencies [[org.clojure/clojure "1.5.1"]]}
                :master {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}}
-  :aliases  {"all" ["with-profile" "+dev:+1.3:+1.4:+1.5:+master"]}
+  :aliases  {"all" ["with-profile" "+dev:+1.4:+1.5:+master"]}
   :global-vars {*warn-on-reflection* true})
