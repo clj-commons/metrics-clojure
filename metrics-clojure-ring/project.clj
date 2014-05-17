fb71f3965d1eb3d4bbaff4af50bf7762654211bf
Michael Klishin <michael@clojurewerkz.org>
2014-05-18 00:29:46 +0400

    2.0.2

--- metrics-clojure-ring/project.clj
+++ metrics-clojure-ring/project.clj
@@ -1,5 +1,5 @@
-(defproject metrics-clojure-ring "2.0.2-SNAPSHOT"
+(defproject metrics-clojure-ring "2.0.2"
   :description "Various things gluing together metrics-clojure and ring."
   :dependencies [[cheshire "5.3.1"]
-                 [metrics-clojure "2.0.1"]]
+                 [metrics-clojure "2.0.2"]]
   :profiles {:dev {:dependencies [[ring "1.2.2"]]}})
