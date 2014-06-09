5d30a8f124e9f54b02d8d838d9ff65b9452043d2
Dev Pairing Station E24778 <E24778@slc.co.uk>
2014-06-09 15:03:01 +0100

    Added suite of wrapper functions for instrumenting jvm metrics

---
+++ metrics-clojure-jvm/project.clj
@@ -0,0 +1,4 @@
+(defproject metrics-clojure-jvm "0.1.0-SNAPSHOT"
+  :description "Gluing together metrics-clojure and jvm instrumentation."
+  :dependencies [[metrics-clojure "2.0.2"]
+                 [com.codahale.metrics/metrics-jvm "3.0.2"]])
---
+++ metrics-clojure-jvm/src/metrics/jvm/core.clj
@@ -0,0 +1,44 @@
+(ns metrics.jvm.core
+  (:import (com.codahale.metrics MetricRegistry JvmAttributeGaugeSet)
+           (com.codahale.metrics.jvm ThreadStatesGaugeSet GarbageCollectorMetricSet FileDescriptorRatioGauge
+                                     MemoryUsageGaugeSet))
+  (:require [metrics.core :refer [add-metric]]))
+
+(defn register-jvm-attribute-gauge-set
+  ([^MetricRegistry reg]
+   (register-jvm-attribute-gauge-set reg ["jvm" "attribute"]))
+  ([^MetricRegistry reg title]
+   (add-metric reg title (new JvmAttributeGaugeSet))))
+
+(defn register-memory-usage-gauge-set
+  ([^MetricRegistry reg]
+   (register-memory-usage-gauge-set reg ["jvm" "memory"]))
+  ([^MetricRegistry reg title]
+   (add-metric reg title (new MemoryUsageGaugeSet))))
+
+(defn register-file-descriptor-ratio-gauge-set
+  ([^MetricRegistry reg]
+   (register-file-descriptor-ratio-gauge-set reg ["jvm" "file"]))
+  ([^MetricRegistry reg title]
+   (add-metric reg title (new FileDescriptorRatioGauge))))
+
+(defn register-garbage-collector-metric-set
+  ([^MetricRegistry reg]
+   (register-garbage-collector-metric-set reg ["jvm" "gc"]))
+  ([^MetricRegistry reg title]
+   (add-metric reg title (new GarbageCollectorMetricSet))))
+
+(defn register-thread-state-gauge-set
+  ([^MetricRegistry reg]
+   (register-thread-state-gauge-set reg ["jvm" "thread"]))
+  ([^MetricRegistry reg title]
+   (add-metric reg title (new ThreadStatesGaugeSet))))
+
+(defn instrument-jvm
+  [^MetricRegistry reg]
+  (doseq [register-metric-set [register-jvm-attribute-gauge-set
+                               register-memory-usage-gauge-set
+                               register-file-descriptor-ratio-gauge-set
+                               register-garbage-collector-metric-set
+                               register-thread-state-gauge-set]]
+    (register-metric-set reg)))
