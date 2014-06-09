778b2ab35fd0c82b64659d5d56f8e7b7bda78878
Michael Klishin <michael@clojurewerkz.org>
2014-06-09 22:28:22 +0400

    master is 2.1.0-SNAPSHOT

--- metrics-clojure-jvm/project.clj
+++ metrics-clojure-jvm/project.clj
@@ -1,4 +1,4 @@
 (defproject metrics-clojure-jvm "2.1.0-SNAPSHOT"
   :description "Gluing together metrics-clojure and jvm instrumentation."
-  :dependencies [[metrics-clojure "2.0.2"]
+  :dependencies [[metrics-clojure "2.1.0-SNAPSHOT"]
                  [com.codahale.metrics/metrics-jvm "3.0.2"]])
--- metrics-clojure-ring/project.clj
+++ metrics-clojure-ring/project.clj
@@ -1,5 +1,5 @@
 (defproject metrics-clojure-ring "2.1.0-SNAPSHOT"
   :description "Various things gluing together metrics-clojure and ring."
   :dependencies [[cheshire "5.3.1"]
-                 [metrics-clojure "2.0.0"]]
+                 [metrics-clojure "2.1.0-SNAPSHOT"]]
   :profiles {:dev {:dependencies [[ring "1.2.2"]]}})
