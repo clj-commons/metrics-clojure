d41dc3e6aa08ac4ace134715f058bb996c4f1308
Michael Klishin <michael@clojurewerkz.org>
2014-06-14 03:43:46 +0400

    Back to dev version

--- metrics-clojure-jvm/project.clj
+++ metrics-clojure-jvm/project.clj
@@ -1,4 +1,4 @@
-(defproject metrics-clojure-jvm "2.1.0"
+(defproject metrics-clojure-jvm "2.1.1-SNAPSHOT"
   :description "Gluing together metrics-clojure and jvm instrumentation."
-  :dependencies [[metrics-clojure "2.1.0"]
+  :dependencies [[metrics-clojure "2.1.1-SNAPSHOT"]
                  [com.codahale.metrics/metrics-jvm "3.0.2"]])
