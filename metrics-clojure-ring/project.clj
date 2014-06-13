90e48bd14198b59e5110c8aaccb865dbaafc5752
Michael Klishin <michael@clojurewerkz.org>
2014-06-14 03:39:46 +0400

    Test against ring 1.3

--- metrics-clojure-ring/project.clj
+++ metrics-clojure-ring/project.clj
@@ -2,4 +2,4 @@
   :description "Various things gluing together metrics-clojure and ring."
   :dependencies [[cheshire "5.3.1"]
                  [metrics-clojure "2.1.0-SNAPSHOT"]]
-  :profiles {:dev {:dependencies [[ring "1.2.2"]]}})
+  :profiles {:dev {:dependencies [[ring "1.3.0"]]}})
