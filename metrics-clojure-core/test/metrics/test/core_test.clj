clj-commons/metrics-clojure

commit 2ea88520dea284f3f5ca5395fb8a30f0db9e7fb0

Counters test now passes

metrics-clojure-core/src/metrics/core.clj
@@ -1,19 +1,24 @@
 (ns metrics.core
-  (:require [metrics.utils :refer [metric-name]])
-  (:import com.yammer.metrics.Metrics
-           java.util.concurrent.TimeUnit
-           com.yammer.metrics.reporting.ConsoleReporter))
+  (:import [com.codahale.metrics MetricRegistry]))
 
+(def ^{:tag "MetricRegistry" :doc "Default registry used by public API functions when no explicit registry argument is given"}
+  default-registry
+  (MetricRegistry.))
+
+(defn ^String metric-name
+  [title]
+  (if (string? title)
+    (MetricRegistry/name "default"
+                         ^"[Ljava.lang.String;" (into-array String ["default" ^String title]))
+    (MetricRegistry/name
+     ^String (first title)
+     ^"[Ljava.lang.String;" (into-array String
+                                        [(second title)
+                                         (last title)]))))
 
 (defn remove-metric
   "Remove the metric with the given title."
-  [title]
-  (.removeMetric (Metrics/defaultRegistry) (metric-name title)))
-
-
-(defn report-to-console
-  "Report all metrics to standard out every few seconds."
-  [seconds]
-  (ConsoleReporter/enable seconds TimeUnit/SECONDS))
-
-
+  ([title]
+     (remove-metric default-registry title))
+  ([^MetricRegistry reg title]
+     (.remove reg (metric-name title))))

metrics-clojure-core/src/metrics/counters.clj
@@ -1,11 +1,9 @@
 (ns metrics.counters
-  (:require [metrics.utils :refer [metric-name desugared-title]])
-  (:import com.yammer.metrics.Metrics
-           com.yammer.metrics.core.Counter
+  (:require [metrics.core  :refer [default-registry]]
+            [metrics.utils :refer [metric-name desugared-title]])
+  (:import [com.codahale.metrics MetricRegistry Counter]
            java.util.concurrent.TimeUnit))
 
-
-; Create ----------------------------------------------------------------------
 (defn counter
   "Create and return a new Counter metric with the given title.
 
@@ -15,8 +13,10 @@
       [\"myapp\" \"webserver\" \"connections\"]
 
   "
-  [title]
-  (Metrics/newCounter (metric-name title)))
+  ([title]
+     (counter default-registry title))
+  ([^MetricRegistry reg title]
+     (.counter reg (metric-name title))))
 
 
 (defmacro defcounter
@@ -33,36 +33,39 @@
     (defcounter [\"a\" \"b\" \"c\"])
     (defcounter [a \"b\" c])
   "
-  [title]
-  (let [[s title] (desugared-title title)]
-    `(def ~s (counter '~title))))
+  ([title]
+     (let [[s title] (desugared-title title)]
+       `(def ~s (counter ~default-registry '~title))))
+  ([^MetricRegistry reg title]
+     (let [[s title] (desugared-title title)]
+       `(def ~s (counter ~reg '~title)))))
 
 
-; Read ------------------------------------------------------------------------
 (defn value
   "Return the current value of the counter."
   [^Counter c]
-  (.count c))
+  (.getCount c))
 
 
-; Write -----------------------------------------------------------------------
 (defn inc!
   "Increment the counter by the given amount (or 1 if not specified)."
-  ([^Counter c] (inc! c 1))
+  ([^Counter c]
+     (inc! c 1))
   ([^Counter c n]
-   (.inc c n)
-   c))
+     (.inc c n)
+     c))
 
 (defn dec!
   "Decrement the counter by the given amount (or 1 if not specified)."
-  ([^Counter c] (dec! c 1))
+  ([^Counter c]
+     (dec! c 1))
   ([^Counter c n]
-   (.dec c n)
-   c))
+     (.dec c n)
+     c))
 
 (defn clear!
   "Clear the given counter, resetting its value to zero."
   [^Counter c]
-  (.clear c)
+  (.dec c (.getCount c))
   c)
 

metrics-clojure-core/src/metrics/reporters/console.clj
@@ -1,0 +1,31 @@
+(ns metrics.reporters.console
+  "Console reporting"
+  (:import java.util.concurrent.TimeUnit
+           [com.codahale.metrics Metrics MetricsRegistry ConsoleReporter ConsoleReporter$Builder Clock MetricsFilter]
+           java.io.PrintStream
+           java.util.Locale))
+
+(defn ^ConsoleReporter reporter
+  ([opts]
+     (reporter (Metrics/defaultRegistry)
+               opts))
+  ([^MetricsRegistry reg opts]
+     (let [b (ConsoleReporter$Builder. reg)]
+       (when-let [^PrintStream s (:stream opts)]
+         (.outputTo b s))
+       (when-let [^Locale l (:locale opts)]
+         (.formattedFor b l))
+       (when-let [^Clock c (:clock opts)]
+         (.withClock b c))
+       (when-let [^TimeUnit ru (:rate-unit opts)]
+         (.convertRatesTo b ru))
+       (when-let [^TimeUnit du (:duration-unit opts)]
+         (.convertDurationsTo b du))
+       (when-let [^MetricsFilter f (:filter opts)]
+         (.filter b f))
+       (.build b))))
+
+(defn start
+  "Report all metrics to standard out periodically"
+  [^ConsoleReporter r ^long seconds]
+  (.start r seconds TimeUnit/SECONDS))

metrics-clojure-core/src/metrics/utils.clj
@@ -1,44 +1,37 @@
 (ns metrics.utils
-  (:import com.yammer.metrics.Metrics
-           [com.yammer.metrics.core Sampling MetricName]))
+  (:require [metrics.core :refer [default-registry]])
+  (:import [com.codahale.metrics MetricRegistry Sampling]))
 
 
-(defn get-percentile [^Sampling metric ^double percentile]
+(defn get-percentile
+  [^Sampling metric ^double percentile]
   (-> metric
-    .getSnapshot
-    (.getValue percentile)))
+      .getSnapshot
+      (.getValue percentile)))
 
-(defn get-percentiles [metric percentiles]
+(defn get-percentiles
+  [metric percentiles]
   (zipmap percentiles
           (map (partial get-percentile metric)
                percentiles)))
 
-(defn ^MetricName metric-name [title]
-  (if (string? title)
-    (new MetricName "default" "default" ^String title)
-    (new MetricName
-         ^String (first title)
-         ^String (second title)
-         ^String (last title))))
+(defn metric-name
+  [title]
+  (metrics.core/metric-name title))
 
-(defn all-metrics []
-  (letfn [(parse-name [^MetricName metric-name]
-            (format "%s.%s.%s"
-                    (.getGroup metric-name)
-                    (.getType metric-name)
-                    (.getName metric-name)))
-          (parse-entry [[metric-name metric]]
-            [(parse-name metric-name)
-             metric])]
-    (into {} (map parse-entry (.allMetrics (Metrics/defaultRegistry))))))
+(defn all-metrics
+  ([]
+     (all-metrics default-registry))
+  ([^MetricRegistry reg]
+     (into {} (.getMetrics reg))))
 
 (defn desugared-title
   "Syntactic sugary goodness for defining metrics concisely with macros.
 
-  Returns a vector of: [symbol-to-define MetricName]"
+  Returns a vector of: [symbol-to-define metric-name]"
   [mn]
   (cond
-    (string? mn) [(symbol mn) mn]
-    (symbol? mn) [mn (str mn)]
-    :else [(symbol (last mn))
-           (map str mn)]))
+   (string? mn) [(symbol mn) mn]
+   (symbol? mn) [mn (str mn)]
+   :else [(symbol (last mn))
+          (map str mn)]))

commit 0104f32d1e41a9e2092b5fcad6dff658d1755750

Use a valid :tag for default-registry (was breaking reflection)
Bug was introduced in 2ea88520dea284f3f5ca5395fb8a30f0db9e7fb0

metrics-clojure-core/src/metrics/core.clj
@@ -1,7 +1,7 @@
 (ns metrics.core
   (:import [com.codahale.metrics MetricRegistry Metric]))
 
-(def ^{:tag "MetricRegistry" :doc "Default registry used by public API functions when no explicit registry argument is given"}
+(def ^{:tag MetricRegistry :doc "Default registry used by public API functions when no explicit registry argument is given"}
   default-registry
   (MetricRegistry.))
 

metrics-clojure-core/test/metrics/test/core_test.clj
@@ -1,0 +1,7 @@
+(ns metrics.test.core-test
+  (:require [metrics.core :as core]
+            [clojure.test :refer :all]))
+
+(deftest regression-default-registry-tag
+  (testing "Ensure default-registry no longer has invalid reflection :tag"
+    (.getMeters core/default-registry)))