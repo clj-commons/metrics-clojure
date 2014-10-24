2db23f6974b49aa722f87197b92980592bcc5465
Michael Klishin <michael@clojurewerkz.org>
2014-10-25 01:06:35 +0400

    2.3.1, for real

--- metrics-clojure-ganglia/project.clj
+++ metrics-clojure-ganglia/project.clj
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 (defproject metrics-clojure-ganglia "2.3.1"
   :description "Ganglia reporter integration for metrics-clojure"
   :dependencies [[metrics-clojure "2.3.1"]
-=======
-(defproject metrics-clojure-ganglia "2.4.0-SNAPSHOT"
-  :description "Ganglia reporter integration for metrics-clojure"
-  :dependencies [[metrics-clojure "2.4.0-SNAPSHOT"]
->>>>>>> master
                  [com.codahale.metrics/metrics-ganglia "3.0.2"]])
