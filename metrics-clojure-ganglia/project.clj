5d1225632743358a2209d393391f0b3197c29ba5
Michael Klishin <michael@clojurewerkz.org>
2014-11-07 23:40:51 +0300

    Back to dev version

--- metrics-clojure-ganglia/project.clj
+++ metrics-clojure-ganglia/project.clj
@@ -1,4 +1,4 @@
-(defproject metrics-clojure-ganglia "2.4.0"
+(defproject metrics-clojure-ganglia "2.5.0-SNAPSHOT"
   :description "Ganglia reporter integration for metrics-clojure"
-  :dependencies [[metrics-clojure "2.4.0"]
+  :dependencies [[metrics-clojure "2.5.0-SNAPSHOT"]
                  [io.dropwizard.metrics/metrics-ganglia "3.1.0"]])
