ad398221b9895a9682d333d9526c039f26837ad8
Ragnar Dahlén <r.dahlen@gmail.com>
2015-07-11 23:47:24 +0100

    Riemann reporter integration.
    Expose a clojure interface around the Riemann metrics reporter provided
    by the Riemann Java client library.

---
+++ metrics-clojure-riemann/project.clj
@@ -0,0 +1,6 @@
+(defproject metrics-clojure-riemann "2.6.0-SNAPSHOT"
+  :description "Riemann reporter integration for metrics-clojure"
+  :url "https://github.com/sjl/metrics-clojure"
+  :license {:name "MIT"}
+  :dependencies [[metrics-clojure "2.6.0-SNAPSHOT"]
+                 [com.aphyr/riemann-java-client "0.3.1"]])
