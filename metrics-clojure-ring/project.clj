f2b06d81ee9a70b6ea919117022f60e6dbb01696
Shantanu Kumar <kumar.shantanu@gmail.com>
2015-09-18 08:06:39 +0530

    bump cheshire version to 5.5.0

--- metrics-clojure-ring/project.clj
+++ metrics-clojure-ring/project.clj
@@ -2,6 +2,6 @@
   :description "Various things gluing together metrics-clojure and ring."
   :url "https://github.com/sjl/metrics-clojure"
   :license {:name "MIT"}
-  :dependencies [[cheshire "5.3.1"]
+  :dependencies [[cheshire "5.5.0"]
                  [metrics-clojure "2.6.0-SNAPSHOT"]]
   :profiles {:dev {:dependencies [[ring "1.3.0"]]}})
