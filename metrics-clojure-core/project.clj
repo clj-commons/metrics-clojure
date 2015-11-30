f3e3d9447228383c1464276b803bdc83b2588f59
Michael Klishin <michael@clojurewerkz.org>
2015-11-30 09:33:16 +0300

    Back to dev version

--- metrics-clojure-core/project.clj
+++ metrics-clojure-core/project.clj
@@ -1,4 +1,4 @@
-(defproject metrics-clojure "2.6.0"
+(defproject metrics-clojure "2.7.0-SNAPSHOT"
   :description "A Clojure façade for Coda Hale's metrics library."
   :url "https://github.com/sjl/metrics-clojure"
   :license {:name "MIT"}
--- metrics-clojure-jvm/project.clj
+++ metrics-clojure-jvm/project.clj
@@ -1,6 +1,6 @@
-(defproject metrics-clojure-jvm "2.6.0"
+(defproject metrics-clojure-jvm "2.7.0-SNAPSHOT"
   :description "Gluing together metrics-clojure and jvm instrumentation."
   :url "https://github.com/sjl/metrics-clojure"
   :license {:name "MIT"}
-  :dependencies [[metrics-clojure "2.6.0"]
+  :dependencies [[metrics-clojure "2.7.0-SNAPSHOT"]
                  [io.dropwizard.metrics/metrics-jvm "3.1.2"]])
