c32c268f1321c00b3357c38fbc5578cfc503ea4e
Michael Klishin <michael@clojurewerkz.org>
2015-11-30 09:19:57 +0300

    Test against Clojure 1.6-1.8; depend on 1.7.0

--- metrics-clojure-core/project.clj
+++ metrics-clojure-core/project.clj
@@ -2,7 +2,7 @@
   :description "A Clojure façade for Coda Hale's metrics library."
   :url "https://github.com/sjl/metrics-clojure"
   :license {:name "MIT"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [io.dropwizard.metrics/metrics-core "3.1.2"]]
   :repositories {"repo.codahale.com" "http://repo.codahale.com"
                  ;; to get Clojure snapshots
@@ -12,8 +12,8 @@
                  "sonatype-snapshots" {:url "http://oss.sonatype.org/content/repositories/snapshots"
                                        :snapshots true
                                        :releases {:checksum :fail :update :always}}}
-    :profiles {:1.4    {:dependencies [[org.clojure/clojure "1.4.0"]]}
-               :1.5    {:dependencies [[org.clojure/clojure "1.5.1"]]}
-               :master {:dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]]}}
-  :aliases  {"all" ["with-profile" "+dev:+1.4:+1.5:+master"]}
+    :profiles {:1.6    {:dependencies [[org.clojure/clojure "1.6.0"]]}
+               :1.8    {:dependencies [[org.clojure/clojure "1.8.0-RC2"]]}
+               :master {:dependencies [[org.clojure/clojure "1.8.0-master-SNAPSHOT"]]}}
+  :aliases  {"all" ["with-profile" "+dev:+1.6:+1.8:+master"]}
   :global-vars {*warn-on-reflection* true})
