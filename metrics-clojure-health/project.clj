4702c0dd12adfacec25a98a9ae189469df9662f4
Michael Klishin <michael@clojurewerkz.org>
2016-06-01 03:56:20 +0300

    2.7.0

--- metrics-clojure-health/project.clj
+++ metrics-clojure-health/project.clj
@@ -1,6 +1,6 @@
-(defproject metrics-clojure-health "2.7.0-SNAPSHOT"
+(defproject metrics-clojure-health "2.7.0"
   :description "Gluing together metrics-clojure and healthchecks."
   :url "https://github.com/sjl/metrics-clojure"
   :license {:name "MIT"}  
-  :dependencies [[metrics-clojure "2.7.0-SNAPSHOT"]
+  :dependencies [[metrics-clojure "2.7.0"]
                  [io.dropwizard.metrics/metrics-healthchecks "3.1.2"]])
--- metrics-clojure-riemann/project.clj
+++ metrics-clojure-riemann/project.clj
@@ -1,6 +1,6 @@
-(defproject metrics-clojure-riemann "2.7.0-SNAPSHOT"
+(defproject metrics-clojure-riemann "2.7.0"
   :description "Riemann reporter integration for metrics-clojure"
   :url "https://github.com/sjl/metrics-clojure"
   :license {:name "MIT"}
-  :dependencies [[metrics-clojure "2.7.0-SNAPSHOT"]
+  :dependencies [[metrics-clojure "2.7.0"]
                  [com.aphyr/metrics3-riemann-reporter "0.4.1"]])
