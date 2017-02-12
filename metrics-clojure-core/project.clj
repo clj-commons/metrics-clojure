0619dfd42c231b0e295782d171df23ad2de64e37
Michael Klishin <michael@clojurewerkz.org>
2017-02-12 18:09:55 +0300

    Bump Clojure 1.9 profile to 1.9.0-alpha14

--- metrics-clojure-core/project.clj
+++ metrics-clojure-core/project.clj
@@ -14,7 +14,7 @@
                                        :releases {:checksum :fail :update :always}}}
     :profiles {:1.6    {:dependencies [[org.clojure/clojure "1.6.0"]]}
                :1.7    {:dependencies [[org.clojure/clojure "1.7.0"]]}
-               :1.9    {:dependencies [[org.clojure/clojure "1.9.0-alpha4"]]}
+               :1.9    {:dependencies [[org.clojure/clojure "1.9.0-alpha14"]]}
                :master {:dependencies [[org.clojure/clojure "1.9.0-master-SNAPSHOT"]]}
                :dev    {:global-vars {*warn-on-reflection* true}}}
   :aliases  {"all" ["with-profile" "+dev:+1.6:+1.7:+1.9:+master"]}
