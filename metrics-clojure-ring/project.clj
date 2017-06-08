067dc89b55c8b8c60159cb40949736dd803a111e
Marcus Spiegel <malex@calclab.com>
2017-06-08 12:22:18 +0200

    Prepare Clojars release
    - Rename project names (add group `malesch`)
    - Set version to 2.9.1

--- metrics-clojure-ring/project.clj
+++ metrics-clojure-ring/project.clj
@@ -1,9 +1,9 @@
-(defproject metrics-clojure-ring "2.10.0-SNAPSHOT"
+(defproject malesch/metrics-clojure-ring "2.9.1"
   :description "Various things gluing together metrics-clojure and ring."
   :url "https://github.com/sjl/metrics-clojure"
   :license {:name "MIT"}
   :dependencies [[cheshire "5.7.1"]
-                 [metrics-clojure "2.10.0-SNAPSHOT"]]
+                 [malesch/metrics-clojure "2.9.1"]]
   :profiles {:dev {:global-vars {*warn-on-reflection* true}
                    :dependencies [[ring "1.4.0"]
                                   [ring/ring-mock "0.3.0"]]}})
