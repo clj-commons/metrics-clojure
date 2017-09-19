7089b0c5793e6af3a6f9f948f356ffd0fc353357
Michael Klishin <michael@clojurewerkz.org>
2017-09-19 00:08:26 -0600

    Back to development version

--- metrics-clojure-ganglia/project.clj
+++ metrics-clojure-ganglia/project.clj
@@ -1,7 +1,7 @@
-(defproject metrics-clojure-ganglia "2.10.0"
+(defproject metrics-clojure-ganglia "2.11.0-SNAPSHOT"
   :description "Ganglia reporter integration for metrics-clojure"
   :url "https://github.com/sjl/metrics-clojure"
   :license {:name "MIT"}
   :profiles {:dev {:global-vars {*warn-on-reflection* true}}}
-  :dependencies [[metrics-clojure "2.10.0"]
+  :dependencies [[metrics-clojure "2.11.0-SNAPSHOT"]
                  [io.dropwizard.metrics/metrics-ganglia "3.2.2"]])
--- metrics-clojure-jvm/project.clj
+++ metrics-clojure-jvm/project.clj
@@ -1,7 +1,7 @@
-(defproject metrics-clojure-jvm "2.10.0"
+(defproject metrics-clojure-jvm "2.11.0-SNAPSHOT"
   :description "Gluing together metrics-clojure and jvm instrumentation."
   :url "https://github.com/sjl/metrics-clojure"
   :license {:name "MIT"}
   :profiles {:dev {:global-vars {*warn-on-reflection* true}}}
-  :dependencies [[metrics-clojure "2.10.0"]
+  :dependencies [[metrics-clojure "2.11.0-SNAPSHOT"]
                  [io.dropwizard.metrics/metrics-jvm "3.2.2"]])
